4b7094464b2be2777a0414ace80451e839c0e6c8
agar <37141685+idencosmos@users.noreply.github.com>
2020-03-18 21:37:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function updateSteemArticles(username) {
           category: 'Uncategorized',
           tags,
           author,
-          path: 'source/_posts/_steemit',
+          path: '_steemit/',
         }, false)
       }
     }
@@ -45,7 +45,7 @@ function updateSteemArticles(username) {
           category: 'Uncategorized',
           tags,
           author,
-          path: 'source/_posts/_steemit',
+          path: '_steemit/',
         }, false)
       }   
       return;
