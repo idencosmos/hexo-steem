e0357c648e9e5de99200573a6c4cf03b29b5ed15
agar <37141685+idencosmos@users.noreply.github.com>
2020-03-18 22:54:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function updateSteemArticles(username) {
           title: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“'),
           content,
           date,
-          category: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“').match(/[).+(\])/g),
+          category: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“').match(/^[^[]+\|([^]]+)/gm),
           tags,
           author,
         }, true)
@@ -41,7 +41,7 @@ function updateSteemArticles(username) {
           title: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“'),
           content,
           date,
-          category: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“').match(/[).+(\])/g),
+          category: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“').match(/^[^[]+\|([^]]+)/gm),
           tags,
           author,
         }, true)
